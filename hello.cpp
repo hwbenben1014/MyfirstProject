ad0fdd3c8c57834269303100210b043246434a42
hwbenben1014 <69238806+hwbenben1014@users.noreply.github.com>
2021-09-27 11:15:19 +0800

    Update hello.cpp
    just test GithubDesktop

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
 using namespace std;
+#include<string>
+class MYtest
+{
+public:
+void myprint(int* age,char* name)
+{
+    age = &m_age;
+    name = &m_name;
+}
+
+private:
+int m_age;
+char m_name;
+};
+
 int main()
 {
     cout<<"Hello CPP\n"<<endl;
